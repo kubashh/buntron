d35bc5732d0d33ec4fc8b5f38c50c22c083af148
Jakub <jakubhanula0@gmail.com>
2025-10-15 19:09:02 +0200

    remove buntron.js

--- dev/build.ts
+++ dev/build.ts
@@ -1,12 +1,5 @@
-// const glob = new Bun.Glob(`**/*`)
-
-// for (const path of glob.scanSync(`src`)) {
-//   const text =
-//   console.log(path)
-// }
-
 await Bun.build({
   entrypoints: [`src/buntron.ts`],
-  minify: false,
+  minify: true,
   outdir: `.`,
 })
